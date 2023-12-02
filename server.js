5d39e74a26bce619cba42484f68d2f219cfdbcfa
DiGureev <dianagureev@gmail.com>
2023-12-02 19:13:44 +0200

    path

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from "path";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { router } from './routes/tasks.route.js';
@@ -9,6 +10,7 @@ dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`run on ${process.env.PORT || 3001}`);
@@ -17,4 +19,7 @@ app.listen(process.env.PORT || 3001, () => {
 app.use("/api/planner", router);
 // app.use("/products", p_router);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
