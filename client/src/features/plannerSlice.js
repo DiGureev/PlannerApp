8b1991f3e7b0236876e6cd2b9c49cfb80e736baf
DiGureev <dianagureev@gmail.com>
2023-12-02 19:33:19 +0200

    deploy

--- client/src/features/plannerSlice.js
+++ client/src/features/plannerSlice.js
@@ -8,39 +8,39 @@ const initState = {
     status: ''
 };
 
-// const BASE_URL = 'http://localhost:3001/api/planner';
+const BASE_URL = 'https://my-planner-app-node-redux.onrender.com/api/planner';
 
 export const fetchAll = createAsyncThunk('planner/fetchall', async()=>{
-    const response = await axios.get(`/`)
+    const response = await axios.get(`${BASE_URL}/`)
     return response.data
   })
 
 export const fetchOneDay = createAsyncThunk('planner/fetchone', async(date)=>{
-const response = await axios.get(`/${date}`)
+const response = await axios.get(`${BASE_URL}/${date}`)
 return response.data
 })
 
 export const addTask = createAsyncThunk('planner/addtask', async(data)=>{
 const {date,task} = data
-const response = await axios.post(`/`, {date,task})
+const response = await axios.post(`${BASE_URL}/`, {date,task})
 return response.data
 })
 
 export const editTask = createAsyncThunk('planner/edittask', async(data)=>{
     const {date,oldTask, newTask} = data
-    const response = await axios.put(`/${date}`, {oldTask, newTask})
+    const response = await axios.put(`${BASE_URL}/${date}`, {oldTask, newTask})
     return response.data
 })
 
 export const delTask = createAsyncThunk('planner/deletetask', async(data)=>{
     const {date,task} = data
     console.log(task)
-    const response = await axios.post(`/${date}`, {task})
+    const response = await axios.post(`${BASE_URL}/${date}`, {task})
     return response.data
 })
 
 export const delAllDay = createAsyncThunk('planner/deleteday', async(date)=>{
-    const response = await axios.delete(`/all/${date}`)
+    const response = await axios.delete(`${BASE_URL}/all/${date}`)
     return response.data
 })
 
